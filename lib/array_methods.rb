1db0413909922d29c1bb46904939f04ed02a9dd2
Santiago Molinari <santomolinari@gmail.com>
2019-11-14 16:24:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,22 @@
+require 'pry'
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+array.length.times do |index|
+  if array[index] == value_to_find
+    return index
+  end
+end
+ return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.lenght. 
 end
 
 def find_min_value(array)
   # Add your solution here
 end
+
+# array.length.times { |index|
+#   puts array[index]
+# }
